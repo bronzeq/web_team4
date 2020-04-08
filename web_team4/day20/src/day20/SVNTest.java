f7c4602ddb69aefd9cee36907515e188d3afca74
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-04-08 16:22:23 +0900

    (2020.04.08.)

---
+++ web_team4/day20/src/day20/SVNTest.java
@@ -0,0 +1,12 @@
+package day20;
+
+public class SVNTest {
+	public static void main(String[] args) {
+		System.out.println("4조 조장 이대한");
+		System.out.println("4조 이민영 확인");
+		System.out.println("4조 김범수 확인");
+		System.out.println("4조 이윤호 확인");
+		System.out.println("4조 신민경 확인");
+		System.out.println("4조 이인지 확인");
+	}
+}
