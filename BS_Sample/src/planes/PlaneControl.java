d0c78949f45a0f42ec718e974dd3ae17a1582fa0
LeeYH94 <55489961+LeeYH94@users.noreply.github.com>
2020-05-02 14:44:05 +0900

    입력한 날에 항공기 없으면 다시 입력받게 수정

--- BS_Sample/src/planes/PlaneControl.java
+++ BS_Sample/src/planes/PlaneControl.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import calender.Calender;
+import calender.MainPromptCalender;
 import core.DBConnection;
 
 public class PlaneControl {
@@ -18,13 +20,56 @@ public class PlaneControl {
 	public void getAirplaneList(String departure, String arrival, String userDate, int userTime) {
 		String query = "";
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+		boolean check_plane = true;
+		MainPromptCalender cal = new MainPromptCalender();
 		// time1 == user가 선택한 시간
 		// time2 == 현재시간
 		Date time = new Date();
 		String time1 = userDate;
 		String time2 = format1.format(time);
-
 		int compare = time1.compareTo(time2);
+		
+		try {
+			conn = DBConnection.getConnection();
+			query = "SELECT COUNT(*) FROM PLANE WHERE DEPARTURE_DATE = ? AND DEPARTURE = ?"
+					+ " AND ARRIVAL = ?";
+			pstm = conn.prepareStatement(query);
+			pstm.setString(1, userDate);
+			pstm.setString(2, departure);
+			pstm.setString(3, arrival);		
+			rs = pstm.executeQuery();
+			rs.next();
+			if(rs.getInt(1) == 0) {
+				check_plane = false;
+			}
+			
+		} catch (SQLException sqle) {
+			System.out.println(sqle);
+			System.out.println("getAirplaneList() 쿼리문 오류");
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("알 수 없는 오류(getAirplaneList())");
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+		if (!check_plane) {
+			System.out.println(userDate + "에 출발하는 비행기가 없습니다.");
+			System.out.println("출발일을 다시 입력해주세요");
+			userDate = cal.runPROMPT();
+			getAirplaneList(departure, arrival, userDate, userTime);
+		}
 		if (compare > 0) {
 			// 선택한 날이 현재 날보다 이후일 때
 			query = "SELECT * FROM PLANE WHERE DEPARTURE_DATE = \'" + userDate + "\'"
@@ -33,6 +78,10 @@ public class PlaneControl {
 		} else if (compare < 0) {
 			// 선택한 날이 현재 날보다 이전일때
 			System.out.println(time2 + " 이전의 날을 입력했습니다.");
+			System.out.println("출발일을 다시 입력해주세요");
+			userDate = cal.runPROMPT();
+			getAirplaneList(departure, arrival, userDate, userTime);
+			
 		} else {
 			// 선택한 날이 현재 날과 같을 때
 			query = "SELECT * FROM PLANE WHERE DEPARTURE_DATE = \'" + userDate + "\' "
@@ -58,7 +107,6 @@ public class PlaneControl {
 						+ rs.getString(4) + "\t" + rs.getString(5) + "\t" + rs.getInt(6) + "\t" + rs.getString(7));
 			}
 			System.out.println("-----------------------------------------------------------------------");
-			pstm.close();
 		} catch (SQLException sqle) {
 			System.out.println(sqle);
 			System.out.println("showAirplaneList() 쿼리문 오류");
